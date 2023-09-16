8522407786f611ee164fe624196b57fb33703ab4
VitaPerevoznikova <frayvita@gmail.com>
2023-09-16 19:31:50 +0300

    summary

---
+++ src/js/fetchImg.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+
+export async function fetchImages(name) {
+  try {
+    let page = 1;
+    let limit = 40;
+
+    const params = new URLSearchParams({
+      page: page,
+      per_page: limit,
+    });
+
+    const url = `https://pixabay.com/api/?key=39403201-abd58df21a454f128cd9be12a&q=${name}&${params}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    const response = await axios.get(url);
+    const responseFormat = await response.data;
+    page += 1;
+    return responseFormat;
+  } catch (error) {
+      console.error(error);
+    }
+}
